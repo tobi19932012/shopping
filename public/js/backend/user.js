8d1ee1cc7acb7b3266099ec1ad098f62c5076c40
tobi19932012 <tobi19932012@gmail.com>
2024-12-28 16:38:28 +0700

    update manager User

--- public/js/backend/user.js
+++ public/js/backend/user.js
@@ -1,16 +1,20 @@
 $(document).ready(function () {
     $('.delete').click(function () {
-        var id = $(this).data('id');
-        let $form = $("<form>", {
-            method: "post",
-            action: "/admin/user/delete" // URL bạn muốn gửi form đến
-        });
-        $form.append($("<input>", {
-            type: "hidden",
-            name: "user_id",
-            value: id // Dữ liệu mẫu
-        }));
-        $("body").append($form);
-        $form.submit();
+        if (confirm('Bạn có muốn bản ghi này không?')) {
+            var id = $(this).data('id');
+            let $form = $("<form>", {
+                method: "post",
+                action: "/admin/user/delete" // URL bạn muốn gửi form đến
+            });
+            $form.append($("<input>", {
+                type: "hidden",
+                name: "user_id",
+                value: id // Dữ liệu mẫu
+            }));
+            $("body").append($form);
+            $form.submit();
+        } else {
+            console('Không xóa user');
+        }
     });
 });
